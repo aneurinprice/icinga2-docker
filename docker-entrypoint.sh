9833be11e8411b5fc6070b8af43dafca57a74af6
Aneurin Price <adp@nyeprice.space>
2022-01-30 23:08:31 +0000

    moo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,7 +15,7 @@ fi
 if [ "${CONFIG_GIT_REPO}" ]; then
 	export GIT_SSH_COMMAND="ssh -i /var/lib/icinga2/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 	rm -rf /etc/icinga2/*
-	git clone ${CONFIG_GIT_REPO} /etc/icinga2/*
+	git clone ${CONFIG_GIT_REPO} /etc/icinga2/
 else
 	echo "NO GIT REPO FOUND, CHECK 'CONFIG_GIT_REPO'"
 	exit 1
