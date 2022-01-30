7f53a48dc1ef57d9100f19e7bc03f8097f372cba
Aneurin Price <adp@nyeprice.space>
2022-01-30 23:02:15 +0000

    ssh buggery

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Config Handling
-if [ -f "/data/ssh/id_rsa" ]; then
+if [ -f "/var/lib/icinga2/.ssh/id_rsa" ]; then
 	echo "SSH Key Configured"
 else
 	echo "No SSH Key detected, Generating"
-	ssh-keygen -b 2048 -t rsa -f /data/ssh/id_rsa -q -N ""
+	ssh-keygen -b 2048 -t rsa -f /var/lib/icinga2/.ssh/id_rsa -q -N ""
 	echo "SSH Keygen Successful, add this to your SCM:"
 	echo "############################################"
-	cat /data/ssh/id_rsa.pub
+	cat /var/lib/icinga2/.ssh/id_rsa.pub
 	echo "############################################"
 
 fi
 if [ "${CONFIG_GIT_REPO}" ]; then
-	export GIT_SSH_COMMAND="ssh -i /data/ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-	rm -rf /data/config/*
-	git clone ${CONFIG_GIT_REPO} /data/config
+	export GIT_SSH_COMMAND="ssh -i /var/lib/icinga2/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+	rm -rf /etc/icinga2/*
+	git clone ${CONFIG_GIT_REPO} /etc/icinga2/*
 else
 	echo "NO GIT REPO FOUND, CHECK 'CONFIG_GIT_REPO'"
 	exit 1
